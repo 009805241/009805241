3699034b42908bcd59a08651cead518c49597a1c
009805241 <103441385+009805241@users.noreply.github.com>
2022-04-12 10:42:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+// TO DO:
+// 1. program generates a random number between 1 and 6.
+// 2. program can optionally generate a selectable amount of numbers between 1 and 6.
+
+namespace DxE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dice x Emulator\n");
+            Console.Title = "DxE";
+
+            //Console.TreatControlCAsInput = true;
+            ConsoleKeyInfo UserInput;
+
+            Console.WriteLine("Press Escape to quit. \nPress R to roll \nS for settings");
+
+            do
+            {
+                UserInput = Console.ReadKey();
+                if (UserInput.Modifiers == ConsoleModifiers.Alt) Console.WriteLine("ALT ");
+                if (UserInput.Modifiers == ConsoleModifiers.Control) Console.WriteLine("CONTROL ");
+                if (UserInput.Modifiers == ConsoleModifiers.Shift) Console.WriteLine("SHIFT ");
+
+                switch (UserInput.Key)
+                {
+                    case ConsoleKey.R:
+                        Dice.RollDice();
+                        break;
+
+                    case ConsoleKey.S:
+                        Dice.SettingsMenu();
+                        break;
+                }
+            } while (UserInput.Key != ConsoleKey.Escape);
+        }
+    }
+}
